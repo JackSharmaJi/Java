2b302d27a45000790623e20e32fce9aa11dab3dc
Jack Sharma <ikmucheli@gmail.com>
2017-11-13 07:13:55 +0530

    Someting

--- concurrentCollections/src/Practice.java
+++ concurrentCollections/src/Practice.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class Practice {
     public static void main(String[] args) {
-        
+        CopyOnWriteArrayList cowal = new CopyOnWriteArrayList();
+        cowal.add("a");
+        cowal.add("a");
+        cowal.add("a");
+        System.out.println(cowal);
+        for (Object each  : cowal) {System.out.println("dsf");
+            cowal.remove(each);
+        }
+        System.out.println(cowal);
     }
 }
